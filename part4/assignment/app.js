cedb8d17ba5bf9ff2af9f0453ac2449fe82cbf3f
baekCode <44539224+baekCode@users.noreply.github.com>
2019-08-26 17:12:04 +0900

    Update app.js
    getDayOfWeek function 
    -리팩토링 해볼것

--- part4/assignment/app.js
+++ part4/assignment/app.js
@@ -1,6 +1,38 @@
-function getDayOfWeek(day,lang) {
-    return ['일','월','화','수','목','금','토'][day]
-  }
-  console.log(getDayOfWeek(0,'US'));
+// if 사용, 예외, 조건 모르는 경우 
+// 의문.. 사용자가 2개의 인자값을 인지하고있는건가..?
+function getDayOfWeek1(day,lang){
+    var a; 
+    if(lang === 'US') {
+        a = ['sun','mon','tue','wed','thu','fri','sat']
+    } else {
+        console.log('US 또는 KR 입력해주세요.')
+    }
+	if(lang === 'KR') {
+        a = ['일','월','화','수','목','금','토']
+    } else {
+        console.log('US 또는 KR 입력해주세요.')
+    }
+    return a[day];
+}
 
-  getDayOfWeek(1,'KR')
+getDayOfWeek1(0,'US');
+getDayOfWeek1(0,'KR');
+//예외
+getDayOfWeek1(0,'미국');
+
+
+
+/*
+    리팩토링 해야 할 부분 찾아보기, 
+    1번, a 변수 그대로이지만 언어값에 따라 변환되기?
+    2번, 사용자가 겪을 오류/버그 찾아볼것
+*/
+function getDayOfWeek2(day,lang){
+    var a = ['sun','mon','tue','wed','thu','fri','sat']
+
+    return a[day];
+}
+
+getDayOfWeek2(0,'US');
+
+
