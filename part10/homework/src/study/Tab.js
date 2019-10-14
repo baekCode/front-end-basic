0a24e828c11be3c568bce3fe2c12ea76590df5d0
baekCode <sosaer88@gmail.com>
2019-10-14 15:16:09 +0900

    tab 레이아웃

--- part10/homework/src/study/Tab.js
+++ part10/homework/src/study/Tab.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import './Tab.scss'
+import React, {useState} from 'react';
 
 const Tab = () => {
+    const tabNav = ['메뉴 1 - 가', '메뉴 2 - 나', '메뉴 3 - 다', '메뉴 4 - 다', '메뉴 5 - 라']
+    const tabCon = [
+        {
+            data: '컨텐츠 1'
+        },
+        {
+            data: '컨텐츠 2'
+        },
+        {
+            data: '컨텐츠 3'
+        },
+        {
+            data: '컨텐츠 4'
+        },
+        {
+            data: '컨텐츠 5'
+        }
+    ]
+    const onClickTavNav = (nav) => {
+        return (
+            console.log(nav)
+        )
+    }
+    const onChangeCon = () => {
+        return (
+            console.log()
+        )
+    }
+
+
     return (
-        <div>
-            <ul>
-                <li>탭 메뉴 1</li>
-                <li>탭 메뉴 2</li>
+        <div className={"tab"}>
+            <ul className={"tab-nav"}>
+                {tabNav.map((nav, i) => <li
+                    className={`tab-nav-${i}`}
+                    key={i}
+                    onClick={onClickTavNav}
+                >{nav}</li>)}
             </ul>
+            <div className={"tab-con"}>
+                {tabCon.map((con, i) => <div className={`tab-con-${i}`} key={i}>{con.data}</div>)}
+            </div>
         </div>
     );
 };
