40268a4b46ed244e49c496d79f9a5182f799bff7
baekCode <sosaer88@gmail.com>
2019-10-14 11:27:33 +0900

    part10 homework, 탭 메뉴 구현

---
+++ part10/homework/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Tab from "./study/Tab";
+function App() {
+    return (
+        <div>
+            <Tab/>
+        </div>
+    )
+}
+export default App;
+
+
+
+
---
+++ part10/homework/src/study/Tab.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Tab = () => {
+    return (
+        <div>
+            <ul>
+                <li>탭 메뉴 1</li>
+                <li>탭 메뉴 2</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Tab;
