fa98f9c79f919c033636ae42f301fdca9a67ab6a
baekCode <sosaer88@gmail.com>
2019-10-18 15:10:15 +0900

    탭구현 -이전 다음 버튼 -탭 네비

--- part10/homework_test/src/study/Test.js
+++ part10/homework_test/src/study/Test.js
@@ -3,22 +3,36 @@ import './Test.scss';
 
 const Test = (props) => {
     const [test, setTest] = useState(0);
-    const [activeClass, setActiveClass] = useState('');
+
+    const nextCon = () => {
+        if (test < (props.children.length - 1)) {
+            setTest(test + 1)
+        }
+    };
+    const prevCon  = () => {
+        if (test > 0) {
+            setTest(test - 1);
+        }
+    }
+
 
     return (   
         <div className={'tab'}>
+            <div>
+                <button onClick={prevCon}>이전</button>
+                <button onClick={nextCon}>다음</button>
+            </div>
             <ul className={'tab-nav'}>
                 {props.children.map((list, i)=>{
                     const navClick = (e) => {
                         setTest(i);
-                        setActiveClass(e.currentTarget.id)
                     }
                     return (
                         <li
                             id={`nav-${i}`}
                             key={i}
                             onClick={navClick}
-                            className={activeClass === `nav-${i}` ? 'active' : ''}
+                            className={test === i ? 'active' : ''}
                         >{list.props.nav}</li>
                     )
                 })}                
